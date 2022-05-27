ee8bc3f464eeee4be84ab544b508adb1a6c30d55
brialaws98 <brianalaws98@gmail.com>
2022-05-27 13:41:15 -0400

    Set error messages
    When the user enter the wrong username/password, this function will display an error message.

--- main.js
+++ main.js
@@ -16,11 +16,19 @@ document.addEventListener("DOMContentLoaded", e=>{
         e.preventDefault();
         loginForm.classList.add("form-hidden");
         createAccountForm.classList.remove("form-hidden");
-    })
+    });
 
     document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.remove("form-hidden");
         createAccountForm.classList.add("form-hidden");
-    })
+    });
+
+loginForm.addEventListener("submit", e=> {
+    e.preventDefault();
+
+      //Perform your AJAX/Fetch login
+
+    setFormMessage(loginForm, "error", "Invalid username/password combination");
+});
 })
