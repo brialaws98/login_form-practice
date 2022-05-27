8578730fa6732162c3bfd798445549868b761c3c
brialaws98 <brianalaws98@gmail.com>
2022-05-26 22:33:40 -0400

    Add event for the "Login" link
    When the Login link is CLICKED, the Create form will then be hidden.

--- main.js
+++ main.js
@@ -6,4 +6,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
         loginForm.classList.add("form-hidden");
         createAccountForm.classList.remove("form-hidden");
     })
+
+    document.querySelector(#linkLogin).addEventListener("click", () => {
+        loginForm.classList.remove("form-hidden");
+        createAccountForm.classList.add("form-hidden");
+    })
 })
